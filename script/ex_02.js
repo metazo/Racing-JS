c0a8dffe59ae60ab67a971732937c54a9d47791b
metazo <metaz0412@gmail.com>
2022-09-28 00:54:03 +0200

    Nombre de clicks

---
+++ script/ex_02.js
@@ -0,0 +1,11 @@
+let clickTime = 0;
+
+const compteur = () => {
+    clickTime++;
+    console.log("nombre de click :", clickTime);
+    document.querySelector("footer div").innerHTML = clickTime;
+}
+
+document.querySelector("footer div").addEventListener("click", compteur);
+
+compteur();
