cad9716c22ebc133fb8dd1ee9a9d160a09f5ca83
Mouhamed Mboup <86803786+metazo@users.noreply.github.com>
2022-10-02 23:07:17 +0200

    Create ex_06.js

---
+++ script/ex_06.js
@@ -0,0 +1,16 @@
+
+
+
+let mage = new Hero("amadeus", "mage", 10, 3);
+let warrior = new Hero("pontius", "warrior", 1, 8);
+
+window.onload = function() {
+
+        mage.toString();
+        warrior.toString();
+
+}
+
+const ucFirst = (string) => {
+    return string.charAt().toUpperCase() + string.slice(1);
+}
